f59bbb56844b81acf5c31842617d3d46988fdb8e
AlexanderC <alexander.moldova@gmail.com>
2015-11-09 12:32:52 +0200

    Make available seamless service proxying

--- src/deep-cache/lib/Cache.js
+++ src/deep-cache/lib/Cache.js
@@ -62,12 +62,17 @@ export class Cache extends Kernel.ContainerAware {
     this._driver = Cache.createDriver(driverName/*, kernel.config.cacheDsn*/);
     this._driver.buildId = kernel.buildId;
 
-    kernel.container.addService(
-      `${this.name}.prototype`,
-      this
-    );
-
     callback();
+  }
+
+  /**
+   * @param {AbstractDriver} target
+   * @param {String} thisArg
+   * @param {*[]} argumentsList
+   * @returns {*}
+   */
+  apply(target, thisArg, argumentsList) {
+    return target[thisArg](...argumentsList);
   }
 
   /**
